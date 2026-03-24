45b2833393f54efb711af8516583ee573c1481a1
zxtni <zxtni@users.noreply.github.com>
2026-03-24 12:58:51 +0530

    Backend: Emergency Admin Recovery

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from core.config import settings
 from api.router import api_router
 
+from core.database import engine, Base, SessionLocal
+from models import user, tournament, wallet 
+from core.security import hash_password
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json"
@@ -28,6 +32,26 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Initialize DB
+Base.metadata.create_all(bind=engine)
+
+# Admin Recovery Block
+db = SessionLocal()
+try:
+    existing_admin = db.query(user.User).filter(user.User.role == "ADMIN").first()
+    if not existing_admin:
+        new_admin = user.User(
+            username="Admin",
+            email="admin@zxtni.in",
+            hashed_password=hash_password("admin123"), # Default password
+            role="ADMIN",
+            is_active=True
+        )
+        db.add(new_admin)
+        db.commit()
+finally:
+    db.close()
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
